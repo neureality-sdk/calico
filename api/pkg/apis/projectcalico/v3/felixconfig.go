a55d42a6e4c1f167a1513c6cc7f321b403e3ad3f
Vadim Eisenberg <vadime@neureality.ai>
2024-09-01 08:48:55 +0000

    add NeuReality copyright

--- api/pkg/apis/projectcalico/v3/felixconfig.go
+++ api/pkg/apis/projectcalico/v3/felixconfig.go
@@ -1,5 +1,6 @@
 // Copyright (c) 2017-2022 Tigera, Inc. All rights reserved.
-
+// Copyright (c) 2024 NeuReality, Ltd. All rights reserved.
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
